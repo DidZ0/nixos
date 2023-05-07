e6a1f037fd2bd89c7c9b78ac1be8e5eb4729c96f
Yan Imensar <yimensar@4cad.fr>
2023-05-07 02:49:28 +0200

    :sparkles: add homemanager config

---
+++ users/bomal/home.nix
@@ -0,0 +1,40 @@
+{ inputs, lib, config, pkgs, ... }: {
+
+    nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  home.username = "bomal";
+  home.homeDirectory = "/home/bomal";
+  home.packages = with pkgs; [
+    neovim
+    git
+    sl
+  ];
+
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  home.stateVersion = "22.11";
+}
