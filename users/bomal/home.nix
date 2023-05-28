0d540adf0b2555aebce8c5071556180fc05117a1
Yan Imensar <yimensar@4cad.fr>
2023-05-28 18:31:56 +0200

    add neovim file browser

--- users/bomal/home.nix
+++ users/bomal/home.nix
@@ -17,6 +17,7 @@
 
   home.packages = with pkgs; [
     (nerdfonts.override {fonts = [ "FiraCode" ]; }) 
+    alacritty
     nnn
     vscode
     logseq
--- users/bomal/nvim/default.nix
+++ users/bomal/nvim/default.nix
@@ -7,12 +7,16 @@
     vimAlias = true;
     vimdiffAlias = true;
     plugins = with pkgs.vimPlugins; [
+      gruvbox-nvim
+      plenary-nvim
       telescope-nvim
+      telescope-fzf-native-nvim
+      telescope-file-browser-nvim
+      which-key-nvim
       nvim-web-devicons
       nvim-lspconfig
       nvim-treesitter.withAllGrammars
-      plenary-nvim
-      gruvbox-nvim
+      copilot-vim
     ];
   };
 
