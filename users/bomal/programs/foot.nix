372cc59f44768255a363f618c8dc9ddfea4a94b5
Yan IMENSAR <sub@yan-imensar.fr>
2024-03-11 00:59:29 +0100

    add waybar

--- users/bomal/programs/foot.nix
+++ users/bomal/programs/foot.nix
@@ -2,5 +2,21 @@
 
 {
   programs.foot.enable = true;
+  programs.foot.settings = {
+    main = { 
+    	term = "foot";
+	font = "FiraCode Nerd Font:size=12";
+	dpi-aware = "yes";  
+	pad = "20x0";
+    };  
+
+    mouse = {
+        hide-when-typing = "yes";
+    };
+    colors = {
+        alpha = "0.90";
+	background = "000000";
+    };
+  };
 }
 
